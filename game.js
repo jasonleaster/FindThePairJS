50923f187a467eb720d17034de79baf60dec3a62
jasonleaster <jasonleaster@gmail.com>
2017-11-11 14:15:36 +0800

    add win-info message

--- game.js
+++ game.js
@@ -4,40 +4,46 @@
  * File  : game.js
  */
 
+Util = {
+    nowInMilSec : function () {
+        return new Date().getTime() / 1000;
+    },
 
+    shuffle: function shuffle(array) {
+        let currentIndex   = array.length;
+        let randomIndex    = 0;
+        let temporaryValue = 0;
 
-/**
- * @param  {[type]}
- * @return {[type]}
- */
-function shuffle(array) {
-    let currentIndex   = array.length;
-    let randomIndex    = 0;
-    let temporaryValue = 0;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue      = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex]  = temporaryValue;
+        }
 
-        // And swap it with the current element.
-        temporaryValue      = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex]  = temporaryValue;
+        return array;
     }
 
-    return array;
 }
+
 
 App = {
     init: function () {
         this.flipCounter = {};
         this.preCard = null;
+        this.flippedOverCards = 0
+        this.totalCards  = 0;
 
         this.loadImages();
         this.bindEvent();
+
+        this.startTime = Util.nowInMilSec();
 
         console.log("Game Initlization finished! Just Enjoy it :D ");
     },
@@ -55,7 +61,7 @@ App = {
         }
 
         let frontFaces = $('.card .front');
-        frontFaces = shuffle(frontFaces);
+        frontFaces = Util.shuffle(frontFaces);
 
         let appCtx = this;
         frontFaces.each(function (index, item) {
@@ -65,9 +71,17 @@ App = {
 
             appCtx.flipCounter[getImgName(imgIdx)] = 0;
         });
+
+        this.totalCards = frontFaces.length;
     },
     bindEvent: function () {
         const appCtx = this;
+
+        /* 
+            try delay the reation for waiting 
+            the UI animation to be finished
+        */
+        const HANDLER_DELAY = 1200;
 
         const handler = function(curCard) {
 
@@ -77,10 +91,27 @@ App = {
                 appCtx.preCard = curCard;
             } else if (appCtx.preCard === curCard) {
                 appCtx.preCard = null;
+                
+                appCtx.flippedOverCards += 2;
+
+                //check if game over appCtx.totalCards
+                if (appCtx.flippedOverCards >= 0) {
+                    console.log("Game OVer! You win the game!");
+
+                    let costTime = Util.nowInMilSec() - appCtx.startTime;
+                    $('.costTime').html(costTime.toFixed(2));
+
+                    $('.win-info').css('visibility', 'visible');
+                }
+                
             } else {
                 appCtx.flipCounter[curCard] = 0;
                 appCtx.flipCounter[appCtx.preCard] = 0;
 
+                /*
+                    reset the cards which are flippered and 
+                    not the same of front faces.
+                 */
                 $('.front[name=' + curCard + ']')
                     .parent().each(function(i, item) {
 
@@ -109,12 +140,12 @@ App = {
 
                     setTimeout(function () {
                         handler(curCard)
-                    }, 1200);
+                    }, HANDLER_DELAY);
                 }
             });
         });
 
-        let body = $('body');
+        let body   = $('body');
         let circle = $('div.center-circle');
         circle.on('click', function () {
             body.addClass('ingame');
